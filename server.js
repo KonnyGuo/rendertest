3b6fed4abd6fb33bf809660f11e9bf7b445034bb
KonnyGuo <guokonny@gmail.com>
2023-08-25 23:28:54 -0400

    first

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const {MongoClient, ObjectId } = require('mongodb')
+require('dotenv').config()
+
+let db,
+    dbConnectionStr = process.env.connectionStr,
+    dbName = 'sample_mflix',
+    collection
+
+MongoClient.connect(dbConnectionStr)
+    .then(client => {
+        console.log(`Connected to database`)
+        db = client.db(dbName)
+        collection = db.collection('movies')
+    })
+
+//middleware
+app.use(express.urlencoded({extended : true}))
+app.use(express.json())
+app.use(cors())
+
+app.get("/search", async (request,response) => {
+    try {
+        let result = await collection.aggregate([
+            {
+                "$search" : {
+                    "autocomplete" : {
+                        "query": `${request.query.query}`,
+                        "path": "title",
+                        "fuzzy": {
+                            "maxEdits":2,
+                            "prefixLength": 3
+                        }
+                    }
+                }
+            }
+        ]).toArray()
+        //console.log(result)
+        response.send(result)
+    } catch (error) {
+        response.status(500).send({message: error.message})
+        //console.log(error)
+    }
+})
+
+app.get("/get/:id", async (request, response) => {
+    try {
+        let result = await collection.findOne({
+            "_id" : new ObjectId(request.params.id)
+        })
+        response.send(result)
+    } catch (error) {
+        response.status(500).send({message: error.message})
+    }
+}
+)
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running.`)
+})
